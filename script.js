7f929ceded324ce36db160c684f8f5f7f34f81f8
PolinaStamenova <polina.stamenova@gmail.com>
2021-06-21 20:46:00 +0300

    Add click event to "mobile-link" in csript.js

--- script.js
+++ script.js
@@ -1,48 +1,72 @@
-let body = document.getElementById("body-project");
-let menu = document.createElement("div");
+const body = document.getElementById("body-project");
+const menu = document.createElement("div");
 menu.className = "menu-style";
 body.appendChild(menu);
 
-let icon = document.createElement("i");
+const icon = document.createElement("i");
 icon.className = "fas fa-times";
 menu.appendChild(icon);
 
-let menuList = document.createElement("ul");
+const menuList = document.createElement("ul");
 menuList.className = "menu-list";
 menu.appendChild(menuList);
 
-let portfolio = document.createElement("li");
+const portfolio = document.createElement("li");
 portfolio.className = "portfolio";
-let portfolioLink = document.createElement("a");
+const portfolioLink = document.createElement("a");
+portfolioLink.className = "mobile-link";
 portfolioLink.textContent = "Portfolio";
 portfolioLink.href = "#portfolio";
 portfolio.appendChild(portfolioLink);
 menuList.appendChild(portfolio);
 
-let about = document.createElement("li");
+const about = document.createElement("li");
 about.className = "about";
-let aboutLink = document.createElement("a");
+const aboutLink = document.createElement("a");
+aboutLink.className = "mobile-link";
 aboutLink.textContent = "About";
 aboutLink.href = "#about";
 about.appendChild(aboutLink);
 menuList.appendChild(about);
 
-let contact = document.createElement("li");
+const contact = document.createElement("li");
 contact.className = "contatc-content";
-let contactLink = document.createElement("a");
+const contactLink = document.createElement("a");
+contactLink.className = "mobile-link";
 contactLink.textContent = "Contact";
 contactLink.href = "#contact";
 contact.appendChild(contactLink);
 menuList.appendChild(contact);
 
-let hamburger = document.getElementById('mob-menu');
-hamburger.addEventListener('click', ()=>{
-  menu.style.display='block';
-  document.getElementById('main').style.filter='blur(5px)';
+const hamburger = document.getElementById("mob-menu");
+hamburger.addEventListener("click", () => {
+  menu.style.display = "block";
+  document.getElementById("main").style.filter = "blur(5px)";
+});
 
-})
+icon.addEventListener("click", () => {
+  menu.style.display = "none";
+  document.getElementById("main").style.filter = "none";
+});
 
-icon.addEventListener('click', ()=>{
-  menu.style.display='none';
-  document.getElementById('main').style.filter='none';
-})
+document
+  .getElementsByClassName("mobile-link")[0]
+  .addEventListener("click", () => {
+    menu.style.display = "none";
+    document.getElementById("main").style.filter = "none";
+    document.getElementsByClassName("card");
+  });
+
+document
+  .getElementsByClassName("mobile-link")[1]
+  .addEventListener("click", () => {
+    menu.style.display = "none";
+    document.getElementById("main").style.filter = "none";
+  });
+
+document
+  .getElementsByClassName("mobile-link")[2]
+  .addEventListener("click", () => {
+    menu.style.display = "none";
+    document.getElementById("main").style.filter = "none";
+  });
