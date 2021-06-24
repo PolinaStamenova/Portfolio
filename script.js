d59589247d77024fb22e116c44aaaecdb4e44d98
PolinaStamenova <polina.stamenova@gmail.com>
2021-06-24 12:56:10 +0300

    merge

--- script.js
+++ script.js
@@ -346,3 +346,11 @@ function seeProjectButton(i) {
 const form = document.getElementById("contact-form");
 const emailInput = document.getElementById("email-address");
 const errorMessage = document.getElementById("error-message");
+
+form.addEventListener("submit", (e) => {
+  if (emailInput.value.toLowerCase() !== emailInput.value) {
+    e.preventDefault();
+    errorMessage.textContent =
+      "Please, use only lowercase ltter for your email!";
+  }
+});
